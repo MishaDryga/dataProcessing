43ec9c99269fe01d80afc37987af08063d30181a
Mkarlos <Mkarlos@DESKTOP-OCD9MAK>
2021-03-04 19:30:04 +0300

    fiveth commit

---
+++ src/main/java/org/obrii/mit/dp2021/drygha/project1/files/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.obrii.mit.dp2021.drygha.project1.files;
+
+/**
+ *
+ * @author 38068
+ */
+public class Config {
+    
+    public static final String FILE_NAME="data.txt";
+    
+}
