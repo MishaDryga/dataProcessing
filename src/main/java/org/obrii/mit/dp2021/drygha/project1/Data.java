8adbd710c7e2580342d8a6b38ecc4685ace8943e
Mkarlos <Mkarlos@DESKTOP-OCD9MAK>
2021-03-05 22:20:10 +0300

    fiveth commit

--- src/main/java/org/obrii/mit/dp2021/drygha/project1/Data.java
+++ src/main/java/org/obrii/mit/dp2021/drygha/project1/Data.java
@@ -1,10 +1,6 @@
 package org.obrii.mit.dp2021.drygha.project1;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 
 import java.io.Serializable;
