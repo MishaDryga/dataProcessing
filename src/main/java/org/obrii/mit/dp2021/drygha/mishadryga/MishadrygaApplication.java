589f27d937b5ed8799738ffffa956dc9ffdf1e7c
Mkarlos <Mkarlos@DESKTOP-OCD9MAK>
2021-05-22 16:11:35 +0300

    fiveth commit

---
+++ src/main/java/org/obrii/mit/dp2021/drygha/mishadryga/MishadrygaApplication.java
@@ -0,0 +1,13 @@
+package org.obrii.mit.dp2021.drygha.mishadryga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishadrygaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishadrygaApplication.class, args);
+	}
+
+}
