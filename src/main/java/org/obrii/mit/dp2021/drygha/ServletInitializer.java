01705dd61075d200fbb9f4ce0805f81fc7a9e6a1
Mkarlos <Mkarlos@DESKTOP-OCD9MAK>
2021-05-22 16:08:12 +0300

    fiveth commit

---
+++ src/main/java/org/obrii/mit/dp2021/drygha/ServletInitializer.java
@@ -0,0 +1,14 @@
+package org.obrii.mit.dp2021.drygha;
+
+import org.obrii.mit.dp2021.drygha.RestJpaConsumerApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestJpaConsumerApplication.class);
+	}
+
+}
